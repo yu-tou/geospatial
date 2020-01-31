cb7d4d14b58093cbb30725211e21039b3cbc5d9f
Orhan Kislal <okislal@apache.org>
2020-01-31 17:28:56 -0500

    analyze3

--- postgis/build/postgis-2.5.2/regress/estimatedextent.sql
+++ postgis/build/postgis-2.5.2/regress/estimatedextent.sql
@@ -1,4 +1,5 @@
 -- #877, #818
+set allow_system_table_mods=true;
 SET client_min_messages TO ERROR;
 create table t(g geometry) DISTRIBUTED BY(g);
 select '#877.1', ST_EstimatedExtent('t','g');
@@ -121,30 +122,25 @@ round(st_ymin(e.e)::numeric, 2), round(st_ymax(e.e)::numeric, 2) from e;
 
 -- test calls with 3th parameter
 
--- delete from p where 't';
--- delete from c1 where 't';
--- delete from c2 where 't';
+delete from p where 't';
+delete from c1 where 't';
+delete from c2 where 't';
 
--- delete from pg_statistic where starelid = 'p'::regclass;
--- delete from pg_statistic where starelid = 'c1'::regclass;
--- delete from pg_statistic where starelid = 'c2'::regclass;
-drop table p cascade;
-
-create table p2(g geometry) DISTRIBUTED BY(g);
-create table c1() inherits (p2);
-create table c2() inherits (p2);
+delete from pg_statistic where starelid = 'p'::regclass;
+delete from pg_statistic where starelid = 'c1'::regclass;
+delete from pg_statistic where starelid = 'c2'::regclass;
 
 analyze c1;
 analyze c2;
-analyze p2;
+analyze p;
 
 -- #3391.13
-with e as ( select ST_EstimatedExtent('public','p2','g','t') as e )
+with e as ( select ST_EstimatedExtent('public','p','g','t') as e )
 select '#3391.13', round(st_xmin(e.e)::numeric, 2), round(st_xmax(e.e)::numeric, 2),
 round(st_ymin(e.e)::numeric, 2), round(st_ymax(e.e)::numeric, 2) from e;
 
 -- #3391.14
-with e as ( select ST_EstimatedExtent('public','p2','g','f') as e )
+with e as ( select ST_EstimatedExtent('public','p','g','f') as e )
 select '#3391.14', round(st_xmin(e.e)::numeric, 2), round(st_xmax(e.e)::numeric, 2),
 round(st_ymin(e.e)::numeric, 2), round(st_ymax(e.e)::numeric, 2) from e;
 
@@ -163,15 +159,15 @@ insert into c1 values ('Point(1 1)'::geometry);
 
 analyze c1;
 analyze c2;
-analyze p2;
+analyze p;
 
 -- #3391.17
-with e as ( select ST_EstimatedExtent('public','p2','g','f') as e )
+with e as ( select ST_EstimatedExtent('public','p','g','f') as e )
 select '#3391.17', round(st_xmin(e.e)::numeric, 2), round(st_xmax(e.e)::numeric, 2),
 round(st_ymin(e.e)::numeric, 2), round(st_ymax(e.e)::numeric, 2) from e;
 
 -- #3391.18
-with e as ( select ST_EstimatedExtent('public','p2','g','t') as e )
+with e as ( select ST_EstimatedExtent('public','p','g','t') as e )
 select '#3391.18', round(st_xmin(e.e)::numeric, 2), round(st_xmax(e.e)::numeric, 2),
 round(st_ymin(e.e)::numeric, 2), round(st_ymax(e.e)::numeric, 2) from e;
 
@@ -185,7 +181,7 @@ with e as ( select ST_EstimatedExtent('public','c1','g', 't') as e )
 select '#3391.20', round(st_xmin(e.e)::numeric, 2), round(st_xmax(e.e)::numeric, 2),
 round(st_ymin(e.e)::numeric, 2), round(st_ymax(e.e)::numeric, 2) from e;
 SET client_min_messages TO NOTICE;
-drop table p2 cascade;
+drop table p cascade;
 SET client_min_messages TO ERROR;
 --
 -- Index assisted extent generation
